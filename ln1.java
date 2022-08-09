9a3ff1b536aeb4c5bd401b598c76765725ab5b3a
Sakshi patil <84581028+sakshi324@users.noreply.github.com>
2022-08-09 10:47:34 +0530

    Add files via upload

---
+++ ln1.java
@@ -0,0 +1,49 @@
+import java.lang.*;
+
+class Base{
+    public int i;
+    public int j;
+
+    public Base(){
+        System.out.println("Inside base constructor");
+    }
+
+    public void fun(){
+        System.out.println("Insisde base fun");
+    }
+
+    public void fun (int x){
+        System.out.println("Inside base fun with integer argument");
+    }
+    public void gun(){
+        System.out.println("Inside base gun");
+    }
+    
+}
+class Derived extends Base{
+    public int x;
+    public int y;
+
+    public Derived(){
+        System.out.println("Inside derived constructor");
+    }
+
+    public void sun(){
+        System.out.println("Inside derived sun");
+    }
+}
+
+class in1 {
+
+    static {
+        System.out.println("Inside static block");
+    }
+    public static void main(String arr[]){
+        System.out.println("Inside main");
+        Derived dobj =new Derived();
+        dobj.fun();
+        dobj.fun(11);
+        dobj.gun();
+        dobj.sun();        
+    }
+}
